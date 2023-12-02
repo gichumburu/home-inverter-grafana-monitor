1589dd6050d2467f04e651e0dab7d71aa5fe5ca1
gichumburu <gichu@mbaruk.com>
2023-12-02 11:14:35 +0300

    disabling influx output

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,6 +1,6 @@
 
-from influxdb import InfluxDBClient
-from influxdb_client.client.write_api import SYNCHRONOUS, ASYNCHRONOUS
+# from influxdb import InfluxDBClient
+# from influxdb_client.client.write_api import SYNCHRONOUS, ASYNCHRONOUS
 
 import os
 from ups import UPS, must_ep3000, must_pv1800, must_ph18_5248
@@ -14,14 +14,14 @@ SUPPORTED_INVERTERS = {
 
 USB_DEVICE = os.environ.get("USB_DEVICE", "/dev/ttyUSB0")
 
-DB_HOST = os.environ.get("DB_HOST", "influxdb")
-DB_PORT = int(os.environ.get("DB_PORT", "8086"))
-DB_USERNAME = os.environ.get("DB_USERNAME", "root")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "root")
-DB_NAME = os.environ.get("DB_NAME", "ups")
+# DB_HOST = os.environ.get("DB_HOST", "influxdb")
+# DB_PORT = int(os.environ.get("DB_PORT", "8086"))
+# DB_USERNAME = os.environ.get("DB_USERNAME", "root")
+# DB_PASSWORD = os.environ.get("DB_PASSWORD", "root")
+# DB_NAME = os.environ.get("DB_NAME", "ups")
 INVERTER_MODEL = os.environ.get("INVERTER_MODEL", "monitor-pv1800")
 
-client = InfluxDBClient(DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME)
+#client = InfluxDBClient(DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME)
 
 if INVERTER_MODEL not in SUPPORTED_INVERTERS:
     print("Unknown inverter model model: {0}".format(INVERTER_MODEL))
@@ -55,4 +55,4 @@ json_body = [
 
 print(json_body)
 
-client.write_points(json_body)
+#client.write_points(json_body)
